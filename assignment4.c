af4add7b94a3aaa566c5fb286de8db11c1c3d115
Simran-99 <68385902+Simran-99@users.noreply.github.com>
2022-07-16 20:30:52 +0530

    Add files via upload
    1. print MySirG 5 times on the screen
    2. print the first 10 natural numbers.
    3. print the first 10 natural numbers in reverse order
    4.  print the first 10 odd natural numbers
    5.  print the first 10 odd natural numbers in reverse order.
    6. print the first 10 even natural numbers
    7. print the first 10 even natural numbers in reverse order
    8. print squares of the first 10 natural numbers
    9. print cubes of the first 10 natural numbers
    10. print a table of 5.

---
+++ assignment4.c
@@ -0,0 +1,102 @@
+//Question 1
+/* #include<stdio.h>
+int main(){
+    for(int i=0;i<5;i++){
+        printf("MysirG\n");
+    }
+} */
+
+//Question 2
+/* #include<stdio.h>
+int main(){
+    for(int i=1;i<=10;i++){
+        printf("%d",i);
+        printf("\n");
+    }
+} */
+
+//Question 3
+
+/* #include<stdio.h>
+int main(){
+    for(int i=10;i>0;i--){
+        printf("%d\n",i);
+    }
+} */
+
+//Question 4
+ /* #include<stdio.h>
+ int main(){
+    for(int i=1;i<20;i=i+2){
+        printf("%d\n",i);
+    }
+ } */
+
+ //Question 5
+
+ /* #include<stdio.h>
+ int main(){
+    for(int i=19;i>0;i=i-2){
+        printf("%d\n",i);
+    }
+ } */
+
+ //Question 6
+
+ /* #include<stdio.h>
+ int main(){
+    for(int i=1;i<=20;i=i+1){
+        if(i%2==0){
+        printf("%d\n",i);
+        }
+    }
+ } */
+
+ //Question 7
+
+ /* #include<stdio.h>
+ int main(){
+    for(int i=20;i>0;i--){
+        if(i%2==0){
+            printf("%d\n",i);
+        }
+    }
+ } */
+
+ //Question 8
+
+/*  #include<stdio.h>
+ int main(){
+    for (int i = 1; i <=10; i++)
+    {
+        printf("%d\n",i*i);
+    }
+    
+ } */
+
+//Question 9
+
+/* #include<stdio.h>
+#include<math.h>
+int main(){
+    int cube;
+    for(int i=1;i<=10;i++){
+        cube=pow(i,3);
+        printf("%d\n",cube);
+
+    }
+    return 0;
+} */
+
+//Question 10
+
+#include<stdio.h>
+int main(){
+    int prod;
+    for(int i=1;i<=10;i++){
+        prod=5*i;
+        printf("5 * %d = %d\n",i,prod);
+
+
+    }
+}
