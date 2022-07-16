64386e2a9b678e8588d6d9e1399eda98528f18fc
Simran-99 <68385902+Simran-99@users.noreply.github.com>
2022-07-16 20:32:29 +0530

    Update assignment4.c

--- assignment4.c
+++ assignment4.c
@@ -1,82 +1,83 @@
 //Question 1
-/* #include<stdio.h>
+#include<stdio.h>
 int main(){
     for(int i=0;i<5;i++){
         printf("MysirG\n");
     }
-} */
+}
+
 
 //Question 2
-/* #include<stdio.h>
+#include<stdio.h>
 int main(){
     for(int i=1;i<=10;i++){
         printf("%d",i);
         printf("\n");
     }
-} */
+}
 
 //Question 3
 
-/* #include<stdio.h>
+#include<stdio.h>
 int main(){
     for(int i=10;i>0;i--){
         printf("%d\n",i);
     }
-} */
+} 
 
 //Question 4
- /* #include<stdio.h>
+  #include<stdio.h>
  int main(){
     for(int i=1;i<20;i=i+2){
         printf("%d\n",i);
     }
- } */
+ } 
 
  //Question 5
 
- /* #include<stdio.h>
+#include<stdio.h>
  int main(){
     for(int i=19;i>0;i=i-2){
         printf("%d\n",i);
     }
- } */
+ }
 
  //Question 6
 
- /* #include<stdio.h>
+#include<stdio.h>
  int main(){
     for(int i=1;i<=20;i=i+1){
         if(i%2==0){
         printf("%d\n",i);
         }
     }
- } */
+ } 
 
  //Question 7
 
- /* #include<stdio.h>
+ #include<stdio.h>
  int main(){
     for(int i=20;i>0;i--){
         if(i%2==0){
             printf("%d\n",i);
         }
     }
- } */
+ } 
 
  //Question 8
 
-/*  #include<stdio.h>
+ #include<stdio.h>
  int main(){
     for (int i = 1; i <=10; i++)
     {
         printf("%d\n",i*i);
     }
     
- } */
+ } 
 
 //Question 9
 
-/* #include<stdio.h>
+ #include<stdio.h>
 #include<math.h>
 int main(){
     int cube;
@@ -86,7 +87,7 @@ int main(){
 
     }
     return 0;
-} */
+} 
 
 //Question 10
 
